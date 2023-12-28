441b9c154cef2db98a769623507de9b03e12ec05
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-28 20:16:58 -0300

    client/feat: navbar component

---
+++ client/src/components/nav-bar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  user: {
+    username: string;
+    avatar: string;
+  };
+}>();
+</script>
+
+<template>
+  <header class="d-flex justify-center">
+    <v-container class="w-100" fluid>
+      <v-row>
+        <v-col cols="12" md="6" class="d-flex justify-center md:justify-start">
+          <div class="d-flex ga-4">
+            <v-avatar :image="user.avatar"></v-avatar>
+            <h3 class="text-uppercase d-flex align-center">
+              {{ user.username }}
+            </h3>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" class="d-flex justify-center md:justify-end">
+          <div class="d-flex ga-4">
+            <v-btn color="#019319" class="text-uppercase"> log </v-btn>
+            <v-btn color="red" class="text-uppercase"> logout </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </header>
+</template>
