016faa113c71ac0eaaf74e1fa839661aefdf7200
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-29 18:55:29 -0300

    fix: config mdi-icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import '@mdi/font/css/materialdesignicons.css';
+
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+  },
   components,
   directives,
   theme: {
