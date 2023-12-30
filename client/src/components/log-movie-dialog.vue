ab7853071efee1e2e64d2e0df6fc020e2ee9de7f
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-30 13:00:00 -0300

    refactor: separate the log movie card of the dialog

--- client/src/components/log-movie-dialog.vue
+++ client/src/components/log-movie-dialog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { UseMovieSearch } from "./../composables/movies.ts";
+import logMovie from "./log-movie.vue";
 import { computed } from "vue";
 
 type movie = {
@@ -13,7 +14,6 @@ type movie = {
 const { getMovies, movies, clearSearch } = UseMovieSearch();
 
 const searchValue = ref("");
-const review = ref("");
 const selectedMovie = ref();
 const isActive = ref(false);
 
@@ -39,33 +39,19 @@ const checkIfMoviesIsEmpty = computed(() => movies.value.length > 0);
 <template>
   <v-dialog width="500">
     <template v-slot:activator="{ props }">
-      <v-btn
-        color="#019319"
-        class="text-uppercase"
-        @click="controlModal"
-        v-bind="props"
-        text="Log"
-      >
+      <v-btn color="#019319" class="text-uppercase" @click="controlModal" v-bind="props" text="Log">
       </v-btn>
     </template>
 
     <template v-slot v-if="isActive">
-      <!-- modal para buscar o filme -->
       <v-card v-if="!selectedMovie" title="Search Movie">
         <v-card-text>
           <v-text-field variant="outlined" v-model="searchValue"></v-text-field>
           <v-card class="mx-0" width="100%">
-            <v-list
-              class="list-item pa-2"
-              v-if="checkIfMoviesIsEmpty"
-              density="comfortable"
-              v-for="movie in movies"
-              :key="movie.id"
-              @click="selectMovie(movie)"
-              >{{ movie.title }} ({{
+            <v-list class="list-item pa-2" v-if="checkIfMoviesIsEmpty" density="comfortable" v-for="movie in movies"
+              :key="movie.id" @click="selectMovie(movie)">{{ movie.title }} ({{
                 movie.release_date.split("-")[0]
-              }})</v-list
-            >
+              }})</v-list>
           </v-card>
         </v-card-text>
 
@@ -77,45 +63,7 @@ const checkIfMoviesIsEmpty = computed(() => movies.value.length > 0);
           </v-btn>
         </v-card-actions>
       </v-card>
-      <!-- need refactor -->
-      <v-card v-else class="pa-5">
-        <v-row>
-          <v-col cols="4">
-            <v-img
-              :src="selectedMovie.poster"
-              alt="Movie Poster"
-              width="250"
-              height="200"
-              contain
-            ></v-img>
-          </v-col>
-          <v-col cols="8">
-            <v-card-title>
-              {{ selectedMovie.title }} ({{
-                selectedMovie.release_date.split("-")[0]
-              }})
-            </v-card-title>
-
-            <v-card-text>
-              <v-textarea label="Digite algo" v-model="review"></v-textarea>
-            </v-card-text>
-
-            <v-card-actions class="d-flex justify-space-between">
-              <v-card-actions class="d-flex flex-grow-1">
-                <v-icon size="small">mdi-star</v-icon>
-                <v-icon size="small">mdi-star</v-icon>
-                <v-icon size="small">mdi-star</v-icon>
-                <v-icon size="small">mdi-star</v-icon>
-                <v-icon size="small">mdi-star</v-icon>
-              </v-card-actions>
-              <v-btn icon="mdi-heart"></v-btn>
-            </v-card-actions>
-            <v-card-actions class="d-flex justify-end">
-              <v-btn color="#019319">save</v-btn>
-            </v-card-actions>
-          </v-col>
-        </v-row>
-      </v-card>
+      <log-movie :selected-movie="selectedMovie" v-else />
     </template>
   </v-dialog>
 </template>
---
+++ client/src/components/log-movie.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts" >
+import { ref } from 'vue';
+
+type movie = {
+    id: number;
+    title: string;
+    release_date: string;
+    poster: string;
+};
+
+defineProps<{
+    selectedMovie: {
+        id: number;
+        title: string;
+        release_date: string;
+        poster: string;
+    }
+}>()
+
+const review = ref('')
+
+function logMovie(selectedMovie: movie) {
+    console.log({
+        ...selectedMovie,
+        review: review.value
+    })
+}
+
+</script>
+
+<template>
+    <v-card class="pa-5">
+        <v-row>
+            <v-col cols="4">
+                <v-img :src="selectedMovie.poster" alt="Movie Poster" width="250" height="200" contain></v-img>
+            </v-col>
+            <v-col cols="8">
+                <v-card-title>
+                    {{ selectedMovie.title }} ({{
+                        selectedMovie.release_date.split("-")[0]
+                    }})
+                </v-card-title>
+
+                <v-card-text>
+                    <v-textarea label="Digite algo" v-model="review"></v-textarea>
+                </v-card-text>
+
+                <v-card-actions class="d-flex justify-space-between">
+                    <v-card-actions class="d-flex flex-grow-1">
+                        <v-icon size="small">mdi-star</v-icon>
+                        <v-icon size="small">mdi-star</v-icon>
+                        <v-icon size="small">mdi-star</v-icon>
+                        <v-icon size="small">mdi-star</v-icon>
+                        <v-icon size="small">mdi-star</v-icon>
+                    </v-card-actions>
+                    <v-btn icon="mdi-heart"></v-btn>
+                </v-card-actions>
+                <v-card-actions class="d-flex justify-end">
+                    <v-btn @click="logMovie(selectedMovie)" color="#019319">save</v-btn>
+                </v-card-actions>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
