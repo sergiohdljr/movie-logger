43370e77a4a20d6d0815e192877690a179ba9089
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-03 12:25:17 -0300

    feat: create user session service

--- server/src/modules/users/service/CreateSessionService.ts
+++ server/src/modules/users/service/CreateSessionService.ts
@@ -1,5 +1,37 @@
-export class CreateSessionService {
-    public async execute(){
+import { AppError } from "@shared/errors/AppErros";
+import { UserRepository } from "../repositories/usersRepository";
+import { TCreateSession, TResponseSession } from "../types/user.types";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { jwt } from "@config/auth";
 
+export class CreateSessionService {
+  public async execute({ email, password }: TCreateSession): Promise<TResponseSession> {
+    const userRepository = new UserRepository();
+
+    const hasUser = await userRepository.findByEmail(email);
+
+    if (!hasUser) {
+      throw new AppError("User not found.", 404);
     }
+
+    const isPasswordCorrect = await compare(password, hasUser.password);
+
+    if (!isPasswordCorrect) {
+      throw new AppError("Password incorrect", 401);
+    }
+
+    if (!jwt.jwt_secret) {
+      throw new AppError("Sign Error", 500);
+    }
+
+    const token = sign({}, jwt.jwt_secret, {
+      subject: hasUser.id,
+      expiresIn: jwt.expiresIn,
+    });
+
+    const RESPONSE = { data: { user: hasUser }, token } satisfies TResponseSession;
+
+    return RESPONSE;
+  }
 }
