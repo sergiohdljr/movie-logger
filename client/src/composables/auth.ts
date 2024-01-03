9d7da25867629e0c307273d912afaf63ab193d8e
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-03 20:22:01 -0300

    feat: UseAuth Composable

---
+++ client/src/composables/auth.ts
@@ -0,0 +1,38 @@
+import { ref } from "vue";
+import { api } from "../config/axios/axios.config";
+import router from "../router";
+
+type LoginPayload = {
+  email: string;
+  password: string;
+};
+
+export function useAuth() {
+  const user = ref();
+  const isAuthenticated = ref(false);
+  const token = ref(localStorage.getItem("token"));
+
+  async function signIn(payload: LoginPayload) {
+    try {
+      const { data } = await api.post("/session", payload);
+      user.value = data.data.user;
+      isAuthenticated.value = true;
+      localStorage.setItem("token", data.token);
+      console.log(user.value);
+    } catch (error) {}
+  }
+
+  function checkAuthentication() {
+    if (token.value) {
+      return true;
+    }
+    return false;
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    location.reload();
+  }
+
+  return { signIn, logout, user, isAuthenticated, checkAuthentication };
+}
