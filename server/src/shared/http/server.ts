6ffba32a39e67cbc796a325cc9f43c8d435c950d
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-02 22:55:42 -0300

    feat: create error middleware

--- server/src/shared/http/server.ts
+++ server/src/shared/http/server.ts
@@ -1,10 +1,23 @@
-import Express from "express";
+import Express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
+import { AppError } from "@shared/errors/AppErros";
 
 const app = Express();
 
 app.use(Express.json());
 app.use(router);
+app.use((error: Error, req: Request, res: Response) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(8080, () => {
   console.log("hello world");
