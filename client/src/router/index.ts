788fbf707f3b265b68b060c29dda18cc11acd17c
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-03 18:51:20 -0300

    feat: login page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import homePageVue from "../views/home-page.vue";
+import loginPageVue from "../views/login-page.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: homePageVue,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: loginPageVue,
   },
 ];
 
---
+++ client/src/views/login-page.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Ref } from "vue";
+import { ref } from "vue";
+
+type LoginPayload = {
+  email: string;
+  password: string;
+};
+
+const email: Ref<string> = ref("");
+const password: Ref<string> = ref("");
+
+function submit(payload: LoginPayload) {
+  console.log(payload);
+  email.value = "";
+  password.value = "";
+}
+</script>
+
+<template>
+  <v-form @submit.prevent="submit({ email, password })">
+    <v-text-field v-model="email" label="email"></v-text-field>
+    <v-text-field v-model="password" label="password"></v-text-field>
+    <v-btn type="submit" block class="mt-2" text="Submit"></v-btn>
+  </v-form>
+</template>
