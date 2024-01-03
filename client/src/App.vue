5316d6b679ac6666f1c29b5a16bbdbf90a2d1937
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-03 18:25:06 -0300

    fix: change components from app to home-page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,7 @@
-<script setup lang="ts">
-import navBar from "./components/nav-bar.vue";
-
-const user = {
-  username: "USERNAME",
-  avatar:
-    "https://a.ltrbxd.com/resized/avatar/upload/1/8/1/7/1/0/5/shard/avtr-0-220-0-220-crop.jpg?v=7a3aa85828",
-};
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <v-app>
-    <nav-bar :user="user" />
     <router-view />
   </v-app>
 </template>
--- client/src/views/home-page.vue
+++ client/src/views/home-page.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import navBar from "../components/nav-bar.vue";
+
+const user = {
+  username: "USERNAME",
+  avatar:
+    "https://a.ltrbxd.com/resized/avatar/upload/1/8/1/7/1/0/5/shard/avtr-0-220-0-220-crop.jpg?v=7a3aa85828",
+};
+</script>
 <template>
-  <p>hello world</p>
+  <nav-bar :user="user" />
 </template>
