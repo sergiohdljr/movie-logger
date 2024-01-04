59746d61cc7813cc73038a5e5802544e4cd16092
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-04 18:22:16 -0300

    feat: create login function

--- client/src/views/login-page.vue
+++ client/src/views/login-page.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
 import { Ref } from "vue";
 import { ref } from "vue";
+import { useAuth } from "../composables/auth";
 
 type LoginPayload = {
   email: string;
   password: string;
 };
 
+const { signIn } = useAuth();
+
 const email: Ref<string> = ref("");
 const password: Ref<string> = ref("");
 
-function submit(payload: LoginPayload) {
-  console.log(payload);
+async function Login(payload: LoginPayload) {
+  try {
+    await signIn(payload);
+    location.reload();
+  } catch (error) {
+    throw new Error("login error");
+  }
+
   email.value = "";
   password.value = "";
 }
 </script>
 
 <template>
-  <v-form @submit.prevent="submit({ email, password })">
+  <v-form @submit.prevent="Login({ email, password })">
     <v-text-field v-model="email" label="email"></v-text-field>
     <v-text-field v-model="password" label="password"></v-text-field>
     <v-btn type="submit" block class="mt-2" text="Submit"></v-btn>
