cc197bb582278c838ed48b82de5b29b976de6c0f
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-04 17:37:17 -0300

    feat: load user profile from api

--- client/src/views/home-page.vue
+++ client/src/views/home-page.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import navBar from "../components/nav-bar.vue";
+import { useAuth } from "../composables/auth";
+import { api } from "../config/axios/axios.config";
+import { ref } from "vue";
 
-const user = {
-  username: "USERNAME",
-  avatar:
-    "https://a.ltrbxd.com/resized/avatar/upload/1/8/1/7/1/0/5/shard/avtr-0-220-0-220-crop.jpg?v=7a3aa85828",
-};
+const imagePlaceholder =
+  "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1704240000&semt=ais";
+
+const { token } = useAuth();
+
+const user = ref({
+  username: "",
+  avatar: imagePlaceholder,
+});
+
+onMounted(async () => {
+  const { data } = await api.get("/profile", {
+    headers: {
+      Authorization: `Bearer ${token.value}`,
+    },
+  });
+
+  user.value.username = data.username;
+});
 </script>
 <template>
   <nav-bar :user="user" />
