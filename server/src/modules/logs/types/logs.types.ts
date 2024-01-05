c1ad7d800f2792ec7f26c55c3c3a61f9f1d4f3c4
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-05 18:17:33 -0300

    fix: create new types for logs

--- server/src/modules/logs/types/logs.types.ts
+++ server/src/modules/logs/types/logs.types.ts
@@ -1,12 +1,17 @@
-import { Log } from "@prisma/client";
+import { Log, Prisma, Movie } from "@prisma/client";
 
 export type TLogRepository = {
-  save: (log: TCreateLog) => Promise<Log>;
+  save: (log: TCreateLogRepo) => Promise<Log>;
   update: (id: string, payload: TUpdateLog) => Promise<Log>;
   findById: (id: string) => Promise<Log | null>;
   findAll: () => Promise<Log[]>;
   remove: (id: string) => Promise<void>;
 };
 
-export type TCreateLog = Omit<Log, "id" | "created_at" | "updated_at">;
+export type TCreateLogRepo = Prisma.LogCreateInput;
+
 export type TUpdateLog = Partial<Omit<Log, "id">>;
+
+export type TCreateLogService = Omit<Log, "id" | "created_at" | "updated_at">;
+
+export type TLogBody = Omit<TCreateLogService, "userId"> & { movie: Movie };
