62cbd8f677315d9040e5f2d0ad7254884ba56ead
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-07 16:39:08 -0300

    refactor: logDiaryService

--- server/src/modules/logs/service/LogDiaryService.ts
+++ server/src/modules/logs/service/LogDiaryService.ts
@@ -1,35 +1,10 @@
 import { LogRepository } from "../repositories/LogRepository";
-import { TLogsResponse } from "../types/logs.types";
-
-type TDiary = {
-  [key: string]: Array<unknown>;
-};
+import { TDiary, TKeyOfTmonthsOfYear, TLogsResponse, monthsOfYear } from "../types/logs.types";
 
 export class LogDiaryService {
   public async execute(id: string): Promise<TLogsResponse<TDiary>> {
     const logRepository = new LogRepository();
     const logs = await logRepository.findAllByUserId(id);
-
-    type TmonthsOfYear = {
-      [key: number]: string;
-    };
-
-    type keyOfTmonthsOfYear = keyof TmonthsOfYear;
-
-    const monthsOfYear: TmonthsOfYear = {
-      0: "January",
-      1: "February",
-      2: "March",
-      3: "April",
-      4: "May",
-      5: "June",
-      6: "July",
-      7: "August",
-      8: "September",
-      9: "October",
-      10: "November",
-      11: "December",
-    } as const;
 
     type keyDiary = keyof TDiary;
 
@@ -49,14 +24,13 @@ export class LogDiaryService {
     };
 
     logs.forEach((log) => {
-      const monthNumber: keyOfTmonthsOfYear = log.movie_watched_date.getMonth();
+      const monthNumber: TKeyOfTmonthsOfYear = log.movie_watched_date.getMonth();
       const monthName: keyDiary = monthsOfYear[monthNumber];
       diary[monthName].push(log);
     });
 
     const response = {
       data: diary,
-      count: logs.length,
     };
 
     return response;
