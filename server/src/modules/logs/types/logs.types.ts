edf1e045ba1996375209bbc265457af1b0634916
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-07 16:39:29 -0300

    feat: create diary types

--- server/src/modules/logs/types/logs.types.ts
+++ server/src/modules/logs/types/logs.types.ts
@@ -19,5 +19,30 @@ export type TLogBody = Omit<TCreateLogService, "userId"> & { movie: Movie };
 
 export type TLogsResponse<T> = {
   data: T;
-  count: number;
+  count?: number;
 };
+
+export type TDiary = {
+  [key: string]: Array<Log>;
+};
+
+export type TMonthsOfYear = {
+  [key: number]: string;
+};
+
+export type TKeyOfTmonthsOfYear = keyof TMonthsOfYear;
+
+export const monthsOfYear: TMonthsOfYear = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+} as const;
