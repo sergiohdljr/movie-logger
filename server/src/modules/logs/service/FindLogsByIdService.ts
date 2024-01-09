072565d6e60bd7d3a9044a7d8ffc50f78e6cc85e
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-09 00:20:56 -0300

    fix: generic TlogsResponse

--- server/src/modules/logs/service/FindLogsByIdService.ts
+++ server/src/modules/logs/service/FindLogsByIdService.ts
@@ -1,12 +1,12 @@
 import { LogRepository } from "../repositories/LogRepository";
-import { TLogsResponse } from "../types/logs.types";
+import { TLogWithMovies, TLogsResponse } from "../types/logs.types";
 
 export class FindLogsByIdService {
-  public async execute(id: string): Promise<TLogsResponse> {
+  public async execute(id: string): Promise<TLogsResponse<TLogWithMovies[]>> {
     const logRepository = new LogRepository();
     const logs = await logRepository.findAllByUserId(id);
 
-    const response: TLogsResponse = {
+    const response: TLogsResponse<TLogWithMovies[]> = {
       data: logs,
       count: logs.length,
     };
