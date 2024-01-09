98fb54fa8e1c1ba1209d5d53ccff3a497b697bff
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-09 00:52:51 -0300

    refactor: home-page

--- client/src/views/home-page.vue
+++ client/src/views/home-page.vue
@@ -7,6 +7,30 @@ import { useAuthStore } from "../store/auth";
 import { storeToRefs } from "pinia";
 import ProfileCard from "../components/profile-card.vue";
 import ButtonsNavigation from "../components/buttons-navigation.vue";
+import { Ref } from "vue";
+import MoviesLogList from "../components/movies-log-list.vue";
+
+type Movie = {
+  id: number;
+  name: string;
+  poster: string;
+  year: string;
+};
+
+type TLogMovie = {
+  contain_spoilers: boolean;
+  created_at: string;
+  had_watched_before: boolean;
+  id: string;
+  like: boolean;
+  movie: Movie;
+  movieId: number;
+  movie_watched_date: string;
+  rating: number;
+  review: string;
+  updated_at: string;
+  userId: string;
+};
 
 const imagePlaceholder =
   "https://a.ltrbxd.com/resized/avatar/upload/1/8/1/7/1/0/5/shard/avtr-0-220-0-220-crop.jpg?v=76021be444";
@@ -20,7 +44,7 @@ const user = ref({
   avatar: imagePlaceholder,
 });
 
-const moviesList = ref();
+const logsList: Ref<TLogMovie[]> = ref([]);
 
 onMounted(async () => {
   const { data } = await api.get("/profile", {
@@ -29,7 +53,7 @@ onMounted(async () => {
     },
   });
 
-  const movies = await api.get("/movies", {
+  const movies = await api.get("/log", {
     headers: {
       Authorization: `Bearer ${token.value}`,
     },
@@ -37,48 +61,14 @@ onMounted(async () => {
 
   user.value.username = data.username;
   user.value.name = data.name;
-  moviesList.value = movies.data;
-
-  console.log(moviesList.value);
+  logsList.value = movies.data.data as TLogMovie[];
 });
 </script>
 <template>
   <nav-bar :user="user" />
   <profile-card :user="user" />
   <buttons-navigation />
-
-  <div
-    class="mt-2 pa-1 text-left border border-outlined d-flex flex-column align-center"
-  >
-    <v-container>
-      <v-row>
-        <!-- Exemplo de 4 Cards em uma linha (3 no código de exemplo abaixo) -->
-        <v-col v-for="movie in moviesList" :key="movie.id" cols="2">
-          <div class="mt-2 pa-1 text-left d-flex flex-column align-center">
-            <!-- V-Card para a imagem -->
-            <v-card width="90" height="135">
-              <v-img
-                class="card-hover"
-                :src="movie.poster"
-                style="width: 100%; height: 100%"
-              ></v-img>
-            </v-card>
-            <!-- Estrelas e ícone de coração -->
-            <div class="d-flex align-items-center mt-2">
-              <div class="d-flex justify-space-between">
-                <span>★</span>
-                <span>★</span>
-                <span>★</span>
-                <span>★</span>
-                <span>★</span>
-              </div>
-              <span class="ml-2" style="font-size: 1em">&#10084;&#65039;</span>
-            </div>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <movies-log-list :logs-list="logsList" />
 </template>
 
 <style>
