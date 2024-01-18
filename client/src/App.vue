87c0ddf7f06ec24e3e573814880cc8c0799f356c
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-18 18:45:11 -0300

    refactor: add toaster component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/toast";
+</script>
 
 <template>
   <div class="container h-screen flex justify-center items-center">
     <router-view />
+    <toaster />
   </div>
 </template>
