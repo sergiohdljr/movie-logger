1b5a565f21895d49b7bd924518614c8bd8df2910
sergiohdljr <sergio.tjf1@gmail.com>
2024-01-30 13:04:03 -0300

    fix: button navigation ui fixes

--- client/src/components/buttons-navigation.vue
+++ client/src/components/buttons-navigation.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { Button } from "./ui/button";
+import { ref, watch } from "vue";
 
-const buttonsValues = ["Films", "Diary"] as const;
-const route = useRouter();
+const buttonsValues = ["Films", "Diary", "Reviews", "Likes"] as const;
+const router = useRouter();
+const route = useRoute();
 
 function setState(state: string) {
-  route.push({ query: { render: state } });
+  router.push({ query: { render: state } });
 }
+
+const renderState = ref("Films");
+
+watch(
+  () => route.query.render,
+  (newState) => {
+    renderState.value = newState as string;
+  }
+);
 </script>
 
 <template>
-  <div class="w-full h-auto flex gap-2">
+  <div
+    class="w-full h-auto flex gap-2 outline outline-steel-blue outline-[0.5px] rounded-sm"
+  >
     <Button
       v-for="(btnText, i) in buttonsValues"
       :key="i"
-      variant="ghost"
-      class="uppercase"
+      :class="`font-normal bg-transparent rounded-none 
+              ${
+                renderState === btnText ? 'border-b-2 border-green' : null
+              }  hover:text-blue-400 hover:bg-transparent`"
       @click="setState(btnText)"
     >
       {{ btnText }}
