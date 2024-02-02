def210a502e7552d6d29358fd2137e35f73cfbcd
sergiohdljr <sergio.tjf1@gmail.com>
2024-02-02 18:58:57 -0300

    refactor: add function log movie on UseMovie hook and and cache data

--- client/src/store/api/movies.ts
+++ client/src/store/api/movies.ts
@@ -2,6 +2,9 @@ import { defineStore } from "pinia";
 import { Ref, ref } from "vue";
 import { TAxiosTypedReturn, api } from "../../config/axios/axios.config";
 import { useAuthStore } from "./auth";
+import { toast } from "@/components/ui/toast";
+import { AxiosError } from "axios";
+import { TLogBody } from "./types";
 
 export const useMovies = defineStore("movies", () => {
   const { token } = useAuthStore();
@@ -20,7 +23,41 @@ export const useMovies = defineStore("movies", () => {
     }
   }
 
-  return { moviesLogged, getLoggedMovies };
+  async function logMovie(logMovie: TLogBody) {
+    try {
+      const { status, data } = await api.post("/log", logMovie, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (status === 200) {
+        moviesLogged.value = [
+          ...moviesLogged.value,
+          { ...data, movie: logMovie.movie },
+        ];
+
+        toast({
+          title: `:)`,
+          description: "Movie was logged with sucess!",
+          variant: "default",
+          class: "bg-green text-white",
+          duration: 2000,
+        });
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast({
+          title: "Log Error",
+          description: error.response?.data.message,
+          variant: "destructive",
+          duration: 2000,
+        });
+      }
+    }
+  }
+
+  return { moviesLogged, getLoggedMovies, logMovie };
 });
 
 type Movie = {
