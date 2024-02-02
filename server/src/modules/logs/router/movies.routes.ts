6a69d1e34e55233548d2683abc82e948211d80cd
sergiohdljr <sergio.tjf1@gmail.com>
2024-02-02 16:10:01 -0300

    fix: remove movies logged

--- server/src/modules/logs/router/movies.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { isAuthenticated } from "@shared/middlewares/isAuthenticated";
-import { LoggedMoviesController } from "../controller/LoggedMoviesController";
-
-export const loggedMoviesRoutes = Router();
-const loggedMoviesController = new LoggedMoviesController();
-
-loggedMoviesRoutes.get("/", isAuthenticated, loggedMoviesController.index);
