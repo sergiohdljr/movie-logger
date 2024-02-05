e76f502265a907c9a2d979fbb3cc070a34153b65
sergiohdljr <sergio.tjf1@gmail.com>
2024-02-05 19:35:55 -0300

    feat: pagination

--- client/src/components/movies-log-list.vue
+++ client/src/components/movies-log-list.vue
@@ -1,37 +1,60 @@
 <script setup lang="ts">
 import movieLogCard from "./movie-log-card.vue";
-
-type Movie = {
-  id: number;
-  name: string;
-  poster: string;
-  year: string;
-};
-
-type TLogMovie = {
-  contain_spoilers: boolean;
-  created_at: string;
-  had_watched_before: boolean;
-  id: string;
-  like: boolean;
-  movie: Movie;
-  movieId: number;
-  movie_watched_date: string;
-  rating: number;
-  review: string;
-  updated_at: string;
-  userId: string;
-};
+import { TLogMovie } from "@/store/api/types";
+import { Button } from "./ui/button";
+import { useMovies } from "@/store/api/movies";
+import { useRoute, useRouter } from "vue-router";
+import { ref, watch } from "vue";
+import { Separator } from "./ui/separator";
 
 defineProps<{
   logsList: TLogMovie[];
+  paginationInfo: {
+    totalMovies: number;
+    actualPage: number;
+    pages: number;
+  };
 }>();
+
+const { getLoggedMovies } = useMovies();
+const router = useRouter();
+const route = useRoute();
+
+function setPageQuery(page: number) {
+  router.push({ query: { page } });
+}
+const page = ref<string | null>();
+
+watch(
+  () => route.query.render,
+  (newState) => {
+    page.value = newState as string;
+  }
+);
+
+const skipPage = async (page: number) => {
+  setPageQuery(page);
+  await getLoggedMovies(page - 1);
+};
 </script>
 
 <template>
-  <div class="movies-grid">
-    <movie-log-card v-for="log in logsList" :log="log" />
-  </div>
+  <main class="w-full h-full">
+    <div class="movies-grid">
+      <movie-log-card v-for="log in logsList" :log="log" />
+    </div>
+    <Separator class="bg-steel-blue" />
+    <div class="w-full h-auto flex gap-3 justify-center pt-2 pb-3">
+      <Button
+        v-for="page in paginationInfo.pages"
+        :disabled="page === paginationInfo.actualPage ? true : false"
+        variant="ghost"
+        class="w-6 h-7 text-steel-blue hover:text-white hover:bg-transparent hover:bg-opacity-70"
+        @click="skipPage(page)"
+        >{{ page }}</Button
+      >
+    </div>
+  </main>
 </template>
 
 <style scoped>
@@ -43,5 +66,6 @@ defineProps<{
   justify-items: center;
   justify-content: center;
   gap: 1.5rem;
+  padding-bottom: 2rem;
 }
 </style>
