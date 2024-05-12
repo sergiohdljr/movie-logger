11ed76bcb282e2aa942c2864f1747ad83b474e96
sergiohdljr <sergio.tjf1@gmail.com>
2024-05-12 16:29:02 -0300

    feat: update profile

--- client/src/components/update-user-dialog.vue
+++ client/src/components/update-user-dialog.vue
@@ -8,20 +8,24 @@ import {
 } from "./ui/alert-dialog";
 import { Button } from "./ui/button";
 import { ref } from "vue";
+import { useUserProfile } from "@/store/api/user";
 
-const usersData = ref({
+const userData = ref({
   name: '',
   username: '',
-  avatar: ""
+  avatar: null
 })
 
+const { updateProfile } = useUserProfile()
+
 const handleAvatar = (e: Event) => {
-  usersData.value.avatar = e.target?.files[0]
+  userData.value.avatar = e.target?.files[0]
 }
 
-const onSubmit = (e: Event) => {
+const onSubmit = async (e: Event) => {
   e.preventDefault()
-  console.log(usersData.value)
+
+  await updateProfile(userData.value)
 }
 
 const dialogOpen = ref<boolean>(false);
@@ -46,14 +50,14 @@ function dialogControl(): void {
             focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50
              dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950
               dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300" type="text" name="name" id="name"
-          placeholder="name" v-model="usersData.name">
+          placeholder="name" v-model="userData.name">
         <input class="flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm 
           ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium
            placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950
             focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800
              dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400
               dark:focus-visible:ring-gray-300" type="text" name="username" id="username" placeholder="username"
-          v-model="usersData.username">
+          v-model="userData.username">
         <input type="file" name="avatar" id="avatar" @change="handleAvatar">
         <AlertDialogFooter>
           <Button variant="destructive" @click="dialogControl">Close</Button>
